9acaf6504c3706404699356a69cac4b6014d3405
Sergio Azconan <sergio.j.azcona@gmail.com>
2022-06-09 15:32:30 -0400

    Added fileas for firt time

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to the number of string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to the number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "Is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -52,22 +59,22 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 # Make sure YOU can explain it that way!
 
-puts 4 < 9
-#YOU DO: Explain.
+puts "is 4 less than than 9?", 4 < 9
+#YOU DO: Explain: the result of this conditional statement comparing the two integers should print: true
 
 books = 3
-puts 4 < books
-# YOU DO: Explain.
+puts "is the number of books less than 4?", 4 < books
+# YOU DO: Explain: compares the varibale "books" to the integer 4; result of this comparison should print: false
 
 friends = 6
 siblings = 2
-puts friends > siblings
-# YOU DO: Explain.
+puts "are the number of friends greater than the number of siblings?", friends > siblings
+# YOU DO: Explain: compares the two variables (friends and siblings) and asks if there are more friends than sublings; should print: true
 
 attendees = 9
 meals = 8
-puts attendees != meals
-# YOU DO: Explain.
+puts "does the number of attendees not equal the number of meals?", attendees != meals
+# YOU DO: Explain: comparing the two varibales (attendees & meals) to know if they do not equal one another; should print: true
 
 
 #-------------------
@@ -79,9 +86,9 @@ finished_homework = false
 
 # EXAMPLE:
 # Determine if the user is hungry and has completed their homework
-is_hungry && finished_homework
+puts is_hungry && finished_homework
 # Determine if the user is hungry or has completed their homework
-is_hungry || finished_homework
+puts is_hungry || finished_homework
 
 loves_to_play = true
 loves_dog_park = false
@@ -90,15 +97,19 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age >= 5
+#i set the dogs age to 5, which is greater than and not equal to a puppy of 1 yr - the variable;
+#should print: false
+
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: #the final line is false becasue one of the conditions is false within that `AND` statement
